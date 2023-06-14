16564b71ecd30f64a74518903799fad0d003831c
brian24C <brianjosuecastro@hotmail.com>
2023-06-14 14:27:58 -0500

    fin de seccion 4

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { HeroComponent } from './hero/hero.component';
+import { ListComponent } from './list/list.component';
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [HeroComponent, ListComponent],
+  declarations: [HeroComponent, ListComponent],
+  providers: [],
+})
+export class HeroModule {}
